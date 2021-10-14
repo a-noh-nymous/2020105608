ec0a2618ece170453e42c425c7affd3c55c8c6cb
Noh <anohnymous@khu.ac.kr>
2021-10-14 20:18:46 +0900

    fixed

--- timetable.cpp
+++ timetable.cpp
@@ -5,11 +5,10 @@ using namespace std;
 int main() {
 	int i = 0;
 	int j = 0;
-	int v = 0;
-	for (i = 1; i < 9; i++) {
-		for (j = 0; j < 0; j++) {
+	for (i = 1; i < 10; i++) {
+		for (j = 0; j < 10; j++) {
 			v = i * j;
-			cout << i << "x" << j << "=" << v << '\t';
+			cout << i << "x" << j << "=" << i * j << '\t';
 		}
 		cout << endl;
 	}
